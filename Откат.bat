af1fefd20512552d086b9fc9585f2a59f625ec35
EnotEric <z6460zivan@vk.com>
2025-04-01 20:56:38 +0300

    Откат (удаление файлов батника)

---
+++ Откат.bat
@@ -0,0 +1,5 @@
+@echo on
+rd /s/q C:\Windows\Temp\Eric-Oboi
+del /s/f/q C:\Windows\Temp\to-go.bat
+del /s/f/q "C:\Windows\Temp\фыр енот фембойчик скачать обои бесплатно без смс и регистрации.bat"
+pause
